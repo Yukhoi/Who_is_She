5c9723d69ae0ab81d316fa7a125f34608a10979b
Enzo Luo <yukai.luo@datategy.net>
2024-12-06 23:28:17 +0100

    get random players

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient,HttpClientModule } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Who_is_She';
+  players: any[] = [];
+  selectedPlayer: any = null;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any>('assets/players/players.json').subscribe(data => {
+      this.players = data.players;
+    });
+  }
+
+  selectRandomPlayer() {
+    if (this.players.length > 0) {
+      const randomIndex = Math.floor(Math.random() * this.players.length);
+      this.selectedPlayer = this.players[randomIndex];
+      console.log(this.selectedPlayer);
+    }
+  }
 }
---
+++ src/app/guessing/guessing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guessing',
+  standalone: true,
+  imports: [],
+  templateUrl: './guessing.component.html',
+  styleUrl: './guessing.component.css'
+})
+export class GuessingComponent {
+
+}
